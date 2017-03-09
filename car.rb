2eade2704a43c328355dedb2d2bbc882078530a9
Sam Lim <limsammy1@gmail.com>
2017-03-09 14:23:18 -0800

    Car can increase and decrease speed

--- car.rb
+++ car.rb
@@ -1,23 +1,46 @@
 # code
+require "pry"
 
 class Car
 
 	attr_reader :wheels, :color
+	attr_accessor :speed
 
-	def initialize
+	def initialize(color = "yellow")
 		# write code that is executed on new instance
 		@wheels = 4
-		@color = "yellow"
+		@color = color
+		@speed = 0
+	end
+
+	def start
+		# pry: stops code for debug
+		# binding.pry
+		self.speed += 50
+	end
+
+	def break
+		# binding.pry
+		self.speed -= 30
 	end
 
 end
 
 
-	############ attr_reader :wheels, :color
+######################### attr_reader :wheels, :color, :speed
 	# def wheels
 	# 	@wheels
 	# end
 
 	# def color
 	# 	@color
+	# end
+
+	# def speed
+	# 	@speed
+	# end
+
+########################## attr_reader + attr_writer = attr_accessor
+	# def speed=(arg)
+	# 	@speed = arg
 	# end
--- car_test.rb
+++ car_test.rb
@@ -27,4 +27,40 @@ class CarTest < Minitest::Test
 		assert_equal("yellow", car.color)
 	end
 
+	def test_it_can_be_any_color
+		car = Car.new("purple")
+
+		assert_equal("purple", car.color)
+	end
+
+	def test_it_has_a_spede_of_0_by_default
+		car = Car.new
+
+		assert_equal(0, car.speed)
+	end
+
+	def test_start_increases_speed_to_50
+		car = Car.new
+
+		assert_equal(0, car.speed)
+
+		car.start
+		
+		assert_equal(50, car.speed)
+	end
+
+	def test_break_decreases_speed_by_30
+		car = Car.new
+
+		assert_equal(0, car.speed)
+
+		car.start
+		
+		assert_equal(50, car.speed)
+
+		car.break 
+
+		assert_equal(20, car.speed)
+	end
+
 end
